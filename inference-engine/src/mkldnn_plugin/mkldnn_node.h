c9c9a4d918cd62bb28d9925b6ab1ef7a9c16c202
Maxim Andronov <maxim.andronov@intel.com>
2021-03-15 10:15:35 +0300

    [CPU] Interpolate node migration on nGraph (#17)

--- inference-engine/src/mkldnn_plugin/mkldnn_node.h
+++ inference-engine/src/mkldnn_plugin/mkldnn_node.h
@@ -661,6 +661,9 @@ public:
     }
 
 protected:
+    bool canBePerformedAsScaleShift() const;
+    bool canFuseSimpleOperation(const MKLDNNNodePtr& node) const;
+
     void setType(Type type) {
         this->type = type;
     }
