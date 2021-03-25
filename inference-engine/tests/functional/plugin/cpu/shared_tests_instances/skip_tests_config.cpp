8ec81714b47b8386934ed558b3404d6b8785d893
dmitrygo <dmitry.gorokhov@intel.com>
2021-03-25 03:13:41 +0300

    fixes after rebase

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -93,6 +93,7 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*SpaceToDepthLayerTest.*)",
         R"(.*StridedSliceLayerTest.*)",
         R"(.*TensorIteratorTest.*)",
+        R"(.*VariadicSplitPad.*)",
 
         // dynamic batch
         R"(.*ReshapeCheckDynBatch.*)",
