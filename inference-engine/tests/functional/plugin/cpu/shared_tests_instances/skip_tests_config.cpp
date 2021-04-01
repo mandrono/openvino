69849fed5eb9342ef2e14666494754fe549b997b
mandrono <maxim.andronov@intel.com>
2021-04-01 11:10:36 +0300

    fixes after rebase

--- inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
+++ inference-engine/tests/functional/plugin/cpu/shared_tests_instances/skip_tests_config.cpp
@@ -133,6 +133,7 @@ std::vector<std::string> disabledTestPatterns() {
         R"(.*SetBlobTest.*)",
         R"(.*SetBlobOfKindTest.*)",
         R"(.*PreprocessTest.*)",
+        ".*PreprocessConversionTest.*",
 
         R"(.*VariableStateTest.*)",
 
